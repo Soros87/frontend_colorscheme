6fd81ba5338b708afd6623fe3e717f1d5b3e7b1c
Soros87 <timkwan87@gmail.com>
2024-01-26 15:15:28 +0800

    set up color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,24 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      "grey-50": "var(--grey-50)",
+      "peach-50": "var(--peach-50)",
+      "sage-50": "var(--sage-50)",
+      "winter-50": "var(--winter-50)",
+
+      "black-50": "var(--black-50)",
+      "black-75": "var(--black-75)",
+      "black-100": "var(--black-100)",
+      "black-125": "var(--black-125)",
+      "black-150": "var(--black-150)",
+
+      "white-50": "var(--white-50)",
+      "white-100": "var(--white-100)",
+      "white-125": "var(--white-125)",
+      "white-150": "var(--white-150)",
     },
+    extend: {},
   },
   plugins: [],
 };
